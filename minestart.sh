cf20c6ee07c14074368b03d317f370e28e78ce5e
pankajmathur <pankymathur@gmail.com>
2018-04-22 02:30:26 -0400

    updated readme and added autominer script

--- minestart.sh
+++ minestart.sh
@@ -1,19 +1,11 @@
+
 #!/bin/bash
-
 DEFAULT_DELAY=0
-
 if [ "x$1" = "x" -o "x$1" = "xnone" ]; then
-
 DELAY=$DEFAULT_DELAY
-
 else
-
 DELAY=$1
-
 fi
-
 sleep $DELAY
-
 cd ethereum-mining-cuda
-
-su pankymathur -c "screen -dmS ethm ./eth.sh"
+su pankymathur -c "screen -dmS ethm ./eth.sh -w 0xA87f228020Ca9D6f4F971c2bda8ad31e87A46D3E"
